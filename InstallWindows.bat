858338e192fb092e4b82dae824049bb069a276ac
K34080 <antoniomarco1484@gmail.com>
2023-06-08 18:16:57 -0500

    Fixes for debugging

--- InstallWindows.bat
+++ InstallWindows.bat
@@ -6,10 +6,14 @@ if errorlevel 1 goto errorNoPython
 
 :: Reaching here means Python is installed.
 :: Execute stuff...
-pip install -R requirements.txt
+pip install -r requirements.txt
 
 :: Once done, exit the batch file -- skips executing the errorNoPython section
-goto:eof
+goto:neof
 
 :errorNoPython
-echo Error^: Python not installed, please make sure you have installed Python 3 and it's added to PATH. It can be obtained at https://www.python.org/
+echo Error^: Python not installed, please make sure you have installed Python 3 and it's added to PATH. It can be obtained at https://www.python.org
+
+:neof
+pause
+goto:eof
